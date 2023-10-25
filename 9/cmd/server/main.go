f6ff5a1c3acd710747a82dcaf2789800da4428ba
Thales Nascimento <thales.silva.nascimento@gmail.com>
2023-10-24 22:17:12 -0300

    feat: add product handler

--- 9/cmd/server/main.go
+++ 9/cmd/server/main.go
@@ -1,8 +1,29 @@
 package main
 
-import "api/config"
+import (
+	"api/config"
+	"api/internal/entity"
+	"api/internal/infra/database"
+	"api/internal/infra/webserver/handlers"
+	"net/http"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
 	conf, _ := config.LoadConfig(".")
 	println(conf.DBDriver)
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+	http.ListenAndServe(":8080", nil)
 }
